25fd023f9a12786d17d4d7c8cc4e1c22bb75c342
kaz <kaz>
2022-07-19 10:26:37 -0400

    v4

--- services/dds_aws_service.py
+++ services/dds_aws_service.py
@@ -33,7 +33,7 @@ def _s3():
     if _k is None or _s is None or _n is None:
         _p('missing credentials')
         _u('{}/bad'.format(STATE_DDS_NOTIFY_CLOUD))
-        return
+        return 1
     _n = 'bkt-' + _n
 
     c = 'AWS_ACCESS_KEY_ID={} AWS_SECRET_ACCESS_KEY={} ' \
@@ -45,10 +45,12 @@ def _s3():
         _u('{}/OK'.format(STATE_DDS_NOTIFY_CLOUD))
         _p('good cloud sync on {}'.format(_t))
         _p(rv.stdout)
-        return
+        return 0
+
     _u('{}/ERR'.format(STATE_DDS_NOTIFY_CLOUD))
-    _p('BAD cloud sync on {}'.format(_t))
+    _p('ERR cloud sync on {}'.format(_t))
     _p(rv.stderr)
+    return 2
 
 
 def main():
@@ -56,25 +58,24 @@ def main():
     ensure_we_run_only_one_instance('dds-aws')
     linux_app_write_pid('dds-aws')
 
-    i = 0
     f = str(get_dds_aws_has_something_to_do_flag())
 
     now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     _p('log created on {}'.format(now))
 
     while 1:
-        if i % 60 == 0:
-            _p('time to do AWS')
-            t = time.perf_counter()
-            _s3()
-            t = time.perf_counter() - t
-            _p('took {} seconds'.format(t))
-        elif os.path.isfile(f):
+        if os.path.isfile(f):
             _p('flag found')
             os.unlink(f)
             _s3()
-        i += 1
-        time.sleep(60)
+            time.sleep(60)
+            continue
+
+        rv = _s3()
+        if rv == 0:
+            time.sleep(150)
+        else:
+            time.sleep(60)
 
 
 # debug
